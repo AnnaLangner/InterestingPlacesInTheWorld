ee06a0ea1e4c255ea852eb2e26e2603409b884df
Ania <annalangner06@gmail.com>
2019-04-05 13:06:34 +0200

    changes in the way of implementing flickity and changes in the functionalities of progress bar and scroll

--- js/script.js
+++ js/script.js
@@ -8,15 +8,26 @@ var flkty = new Flickity( elem, {
   contain: true
 });
 
-var $carousel = $('.main-carousel').flickity();
+var $carousel = $('.main-carousel').flickity({
+	hash: true
+});
 
-$('.bntRestart').on( 'click', function() {
-  $carousel.flickity( 'next', true );
+
+var restartButton = document.querySelector('.bntRestart');
+
+restartButton.addEventListener( 'click', function() {
+  flkty.next( true );
 });
 
 var $progressBar = $('.progress-bar');
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 
 $carousel.on( 'scroll.flickity', function( event, progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   $progressBar.width( progress * 100 + '%' );
 });
+
