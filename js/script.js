f1a7ba26dee2d275884267481729b613f2f7f2cc
Ania <annalangner06@gmail.com>
2019-04-07 12:28:23 +0200

    Changing implementation way for carousel, adding transform style for slides, adding progress bar and removing page dots

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-
-//implementacja karuzeli Flickity
-
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
+var carousel = document.querySelector('.main-carousel');
+var flkty = new Flickity( carousel, {
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  imagesLoaded: true,
+  percentPosition: false,
+  hash: true,
 });
 
-var $carousel = $('.main-carousel').flickity({
-	hash: true
-});
+var imgs = carousel.querySelectorAll('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
 
+flkty.on( 'scroll', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+  });
+});
 
 var restartButton = document.querySelector('.bntRestart');
 
@@ -19,15 +27,9 @@ restartButton.addEventListener( 'click', function() {
   flkty.next( true );
 });
 
-var $progressBar = $('.progress-bar');
+var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
-$carousel.on( 'scroll.flickity', function( event, progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  $progressBar.width( progress * 100 + '%' );
-});
-
